100032a766e65ccef41c02c0f7bb42a545d1bc71
Shiming Zhang <wzshiming@hotmail.com>
2024-09-02 10:47:33 +0800

    ignore the query

--- crproxy.go
+++ crproxy.go
@@ -559,6 +559,8 @@ func (c *CRProxy) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	r.URL.Host = info.Host
 	r.URL.Scheme = c.getScheme(info.Host)
 	r.URL.Path = path
+	r.URL.RawQuery = ""
+	r.URL.ForceQuery = false
 
 	if info.Blobs != "" && c.isRedirectToOriginBlob(r, imageInfo) {
 		c.redirectBlobResponse(rw, r, info)
