50780dcedbb7a3527e11bff82d4f6a56ff6b68a3
Shiming Zhang <wzshiming@hotmail.com>
2024-12-13 14:01:50 +0800

    Add cluster mode

---
+++ gateway/blob.go
@@ -0,0 +1,37 @@
+package gateway
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/daocloud/crproxy/agent"
+	"github.com/daocloud/crproxy/token"
+)
+
+func (c *Gateway) blob(rw http.ResponseWriter, r *http.Request, info *PathInfo, t *token.Token) {
+	if t.Attribute.BlobsURL != "" {
+		values := url.Values{
+			"referer": {r.RemoteAddr},
+		}
+
+		if authorization := r.Header.Get("authorization"); authorization != "" {
+			values.Add("Authorization", authorization)
+		}
+
+		blobURL := fmt.Sprintf("%s/v2/%s/%s/blobs/%s?%s", t.Attribute.BlobsURL, info.Host, info.Image, info.Blobs, values.Encode())
+		http.Redirect(rw, r, blobURL, http.StatusTemporaryRedirect)
+		return
+	}
+
+	if c.agent != nil {
+		c.agent.Serve(rw, r, &agent.BlobInfo{
+			Host:  info.Host,
+			Image: info.Image,
+			Blobs: info.Blobs,
+		}, t)
+		return
+	}
+
+	c.forward(rw, r, info, t)
+}
