e60960caf675bb3ce432440014698aa397060899
Shiming Zhang <wzshiming@hotmail.com>
2025-02-11 10:51:00 +0800

    Add --link-expires for gateway

--- cmd/crproxy/cluster/gateway/gateway.go
+++ cmd/crproxy/cluster/gateway/gateway.go
@@ -32,6 +32,7 @@ type flagpole struct {
 
 	StorageURL    string
 	RedirectLinks string
+	LinkExpires   time.Duration
 	SignLink      bool
 
 	ManifestCacheDuration  time.Duration
@@ -89,6 +90,7 @@ func NewCommand() *cobra.Command {
 	cmd.Flags().StringVar(&flags.BigStorageURL, "big-storage-url", flags.BigStorageURL, "Big storage driver url")
 	cmd.Flags().IntVar(&flags.BigStorageSize, "big-storage-size", flags.BigStorageSize, "Big storage size")
 	cmd.Flags().StringVar(&flags.RedirectLinks, "redirect-links", flags.RedirectLinks, "Redirect links")
+	cmd.Flags().DurationVar(&flags.LinkExpires, "link-expires", flags.LinkExpires, "Link expires")
 	cmd.Flags().BoolVar(&flags.SignLink, "sign-link", flags.SignLink, "Sign Link")
 
 	cmd.Flags().DurationVar(&flags.ManifestCacheDuration, "manifest-cache-duration", flags.ManifestCacheDuration, "Manifest cache duration")
@@ -172,6 +174,10 @@ func runE(ctx context.Context, flags *flagpole) error {
 			return fmt.Errorf("create storage driver failed: %w", err)
 		}
 		cacheOpts = append(cacheOpts, cache.WithStorageDriver(sd))
+
+		if flags.LinkExpires > 0 {
+			cacheOpts = append(cacheOpts, cache.WithLinkExpires(flags.LinkExpires))
+		}
 
 		if flags.RedirectLinks != "" {
 			u, err := url.Parse(flags.RedirectLinks)
